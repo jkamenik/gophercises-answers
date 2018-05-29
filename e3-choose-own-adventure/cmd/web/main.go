abea612799f75c029476817898a1a9ab1c2aceb3
John Kamenik <jkamenik@fourv.com>
2018-05-29 08:18:09 -0400

    Building a default HTTP handler for rendering the page.

--- e3-choose-own-adventure/cmd/web/main.go
+++ e3-choose-own-adventure/cmd/web/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 
 	"../../cyoa"
 )
 
+var tpl *template.Template
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{ range .Paragraphs }}
+    <p>{{.}}</p>
+    {{ end }}
+    
+    <ul>
+    {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+    <ul>
+  </body>
+</html>
+`
+
 func main() {
+	port := flag.Int("port", 3000, "the port to start the server on.")
 	file := flag.String("file", "gopher.json", "the JSON file with the story.")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *file)
@@ -19,11 +45,31 @@ func main() {
 		panic(err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.ParseJSON(f)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := newHandler(story)
+	fmt.Printf("Stargind server on %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
+
+func newHandler(s cyoa.Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s cyoa.Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
 }
--- e3-choose-own-adventure/cyoa/story-arc.go
+++ e3-choose-own-adventure/cyoa/story-arc.go
@@ -1,14 +1,19 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // Story is the main story listing all Chapters.
 type Story map[string]Chapter
 
 // Chapter is part of the main story type.  It contains the title, the story
 // text (as paragraphs), and the other stories that can be selected at the end.
 type Chapter struct {
-	Title     string   `json:"title"`
-	Paragraph []string `json:"story"`
-	Options   []Option `json:"options"`
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
 }
 
 // Option contains the text to show to the user and next chapter title to choose
@@ -16,4 +21,13 @@ type Chapter struct {
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
+}
+
+// ParseJSON parses a JSON like story and returns a story.
+func ParseJSON(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+
+	err := d.Decode(&story)
+	return story, err
 }
